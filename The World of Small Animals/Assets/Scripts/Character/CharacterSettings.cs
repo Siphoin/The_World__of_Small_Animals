4e69ebde3d28d9805dd7f7f4017888d4b308f441
Siphoin <maksimka.kosolapov.01@mail.ru>
2021-08-15 11:27:28 +0500

    Update CharacterSettings.cs

--- The World of Small Animals/Assets/Scripts/Character/CharacterSettings.cs
+++ The World of Small Animals/Assets/Scripts/Character/CharacterSettings.cs
@@ -5,17 +5,17 @@ public class CharacterSettings : ScriptableObject
     {
     [Header("Скорость персонажа")]
     [SerializeField]
-    private float speed = 20;
+    private float _speed = 20;
 
     [Header("Скорость замедления персонажа")]
     [SerializeField]
-    private float speedDecrement = 0.5f;
+    private float _speedDecrement = 0.5f;
 
     [Header("Через какое время игрок будет отключен от сервекра если персонаж не движется")]
     [SerializeField]
-    private float afkTimeOut = 180;
+    private float _afkTimeOut = 180;
 
-    public float Speed { get => speed; }
-    public float SpeedDecrement { get => speedDecrement; }
-    public float AfkTimeOut { get => afkTimeOut; }
+    public float Speed => _speed;
+    public float SpeedDecrement => _speedDecrement; 
+    public float AfkTimeOut => _afkTimeOut;
 }
