338aaa85929ce072721729f3a8a2743099387471
Siphoin <maksimka.kosolapov.01@mail.ru>
2021-08-15 11:52:59 +0500

    Update SmileContainer.cs

--- The World of Small Animals/Assets/Scripts/Others/SmileContainer.cs
+++ The World of Small Animals/Assets/Scripts/Others/SmileContainer.cs
@@ -1,10 +1,10 @@
-﻿using UnityEditor;
-using UnityEngine;
+﻿using UnityEngine;
+
 [CreateAssetMenu(menuName = "Data/UI/Smiles Container", order = 0)]
 public class SmileContainer : ScriptableObject
     {
     [Header("Список доступных смайлов для отправки игроку")]
-    [SerializeField] private Sprite[] smilesSprites;
+    [SerializeField] private Sprite[] _smilesSprites;
 
-    public Sprite[] SmilesSprites { get => smilesSprites; }
+    public Sprite[] SmilesSprites => _smilesSprites;
 }
