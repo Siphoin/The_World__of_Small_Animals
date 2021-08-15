94690790f640d5ae1e3a5eb51f5d174cf203a314
Siphoin <maksimka.kosolapov.01@mail.ru>
2021-08-15 11:57:42 +0500

    Update ListenerCallbacksServer.cs

--- The World of Small Animals/Assets/Scripts/Server/ListenerCallbacksServer.cs
+++ The World of Small Animals/Assets/Scripts/Server/ListenerCallbacksServer.cs
@@ -3,30 +3,27 @@ using Photon.Realtime;
 
 public class ListenerCallbacksServer : MonoBehaviourPunCallbacks
 {
-    private WindowNotfication activeErrorNotfication;
-        // Use this for initialization
+    private WindowNotfication _activeErrorNotfication;
 
     private void CreateErrorNotfication (string textError)
     {
-        if (activeErrorNotfication != null)
+        if (_activeErrorNotfication != null)
         {
             return;
         }
-        activeErrorNotfication = ManagerWindowsNotfications.Manager.CreateNotfication(textError, MessageNotficationType.Error, true);
+        _activeErrorNotfication = ManagerWindowsNotfications.Manager.CreateNotfication(textError, MessageNotficationType.Error, true);
     }
 
     #region Server Callbacks 
 
 
-    public override void OnLeftRoom()
-    {
-        CreateErrorNotfication($"Соединение с локацией было прервано. Проверьте подключение к сети Интернет.");
-    }
 
     public override void OnDisconnected(DisconnectCause cause)
     {
         CreateErrorNotfication($"Соединение с сервером было прервано.\nКод ошибки: {cause} Проверьте подключение к сети Интернет.");
     }
+
+    public override void OnLeftRoom() => CreateErrorNotfication($"Соединение с локацией было прервано. Проверьте подключение к сети Интернет.");
 
     #endregion
 }
