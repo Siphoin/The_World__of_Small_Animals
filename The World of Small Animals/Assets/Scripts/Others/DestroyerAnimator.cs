62650d0e7aaf7a24f7d81a297adfcfd929ceb702
Siphoin <maksimka.kosolapov.01@mail.ru>
2021-08-15 11:50:34 +0500

    Update DestroyerAnimator.cs

--- The World of Small Animals/Assets/Scripts/Others/DestroyerAnimator.cs
+++ The World of Small Animals/Assets/Scripts/Others/DestroyerAnimator.cs
@@ -1,24 +1,23 @@
 ﻿using UnityEngine;
 public class DestroyerAnimator : MonoBehaviour
 {
-
-    private Animator animator;
+    private Animator _animator;
     private void Awake()
     {
-        if (!TryGetComponent(out animator))
+        if (!TryGetComponent(out _animator))
         {
             throw new DestroyerAnimatorException($"{name} not have component Animator");
         }
     }
     public void RemoveAnimator()
     {
-        Destroy(animator);
+        Destroy(_animator);
         Destroy(this);
     }
 
     public void RemoveAnimator(float time)
     {
-        Destroy(animator, time);
+        Destroy(_animator, time);
         Destroy(this, time + 0.02f);
     }
 }
