2e82f2eb6ffe40dd116876bf2246186b7580ea9f
Siphoin <maksimka.kosolapov.01@mail.ru>
2021-08-15 11:52:16 +0500

    Update PanelNameLocation.cs

--- The World of Small Animals/Assets/Scripts/Others/PanelNameLocation.cs
+++ The World of Small Animals/Assets/Scripts/Others/PanelNameLocation.cs
@@ -1,21 +1,14 @@
-﻿using System.Collections;
-using TMPro;
+﻿using TMPro;
 using UnityEngine;
 
-    public class PanelNameLocation : MonoBehaviour
+public class PanelNameLocation : MonoBehaviour
     {
     [Header("Текст имени локации")]
-    [SerializeField] private TextMeshProUGUI textNameLocation;
-        // Use this for initialization
-        void Start()
-    {
-        Ini();
-
-    }
+    [SerializeField] private TextMeshProUGUI _textNameLocation;
 
     private void Ini()
     {
-        if (textNameLocation == null)
+        if (_textNameLocation == null)
         {
             throw new PanelNameLocationException("text name location not seted");
         }
@@ -24,7 +17,10 @@ using UnityEngine;
     public void SetText (string text)
     {
         Ini();
-        textNameLocation.text = text;
+
+        _textNameLocation.text = text;
     }
 
-    }
+    void Start() => Ini();
+
+}
