97e853e75fb066e2439c72c6d25fb82cef77b109
Siphoin <maksimka.kosolapov.01@mail.ru>
2021-08-15 11:31:09 +0500

    Update ListenerLocalCharacter.cs

--- The World of Small Animals/Assets/Scripts/Character/ListenerLocalCharacter.cs
+++ The World of Small Animals/Assets/Scripts/Character/ListenerLocalCharacter.cs
@@ -1,35 +1,33 @@
 ﻿using Photon.Pun;
 using System;
-using System.Collections;
 using UnityEngine;
 
-    public class ListenerLocalCharacter : MonoBehaviour, IFinderLocalPlayer, IInvokerMono
+public class ListenerLocalCharacter : MonoBehaviour, IFinderLocalPlayer, IInvokerMono
     {
-        private const string TAG_MY_PLAYER = "MyPlayer";
+       private const string TAG_MY_PLAYER = "MyPlayer";
 
+       private const string PATH_SETTINGS_CHARACTER = "Data/Character/CharacterSettings";
 
-    private const string PATH_SETTINGS_CHARACTER = "Data/Character/CharacterSettings";
+       private bool _invokeStarted = false;
 
-    private CharacterController myPlayer;
+       private CharacterController _isMyPlayer;
 
-    private CharacterSettings characterSettings;
-
-    private bool invokeStarted = false;
-
+       private CharacterSettings _characterSettings;
 
     // Use this for initialization
     void Start()
         {
 
-        characterSettings = Resources.Load<CharacterSettings>(PATH_SETTINGS_CHARACTER);
+        _characterSettings = Resources.Load<CharacterSettings>(PATH_SETTINGS_CHARACTER);
 
-        if (characterSettings == null)
+        if (_characterSettings == null)
         {
             throw new ListenerLocalCharacterException("character settings not found");
         }
-        myPlayer = FindLocalPlayerWithTag(TAG_MY_PLAYER);
 
-        myPlayer.onMoveStatusChanged += StartInvokingAFK;
+        _isMyPlayer = FindLocalPlayerWithTag(TAG_MY_PLAYER);
+
+        _isMyPlayer.OnMoveStatusChanged += StartInvokingAFK;
 
         StartInvokingAFK(false);
         }
@@ -43,17 +41,18 @@ using UnityEngine;
 
         else
         {
-            if (!invokeStarted)
+            if (!_invokeStarted)
             {
-                invokeStarted = true;
-                CallInvokingMethod(DisconnectOnServer, characterSettings.AfkTimeOut);
+                _invokeStarted = true;
+
+                CallInvokingMethod(DisconnectOnServer, _characterSettings.AfkTimeOut);
             }
         }
     }
 
     private void DisconnectOnServer()
     {
-        if (myPlayer.Moved)
+        if (_isMyPlayer.Moved)
         {
             return;
         }
@@ -68,10 +67,10 @@ using UnityEngine;
     private void StopInvoke()
     {
         CancelInvoke();
-        invokeStarted = false;
+
+        _invokeStarted = false;
     }
 
-    // Update is called once per frame
 
     public CharacterController FindLocalPlayerWithTag(string tag)
     {
