ef0f53f219016a8f58c6639642d523bd425559c9
Maxim <36982547+Siphoin@users.noreply.github.com>
2022-04-17 15:15:00 +0500

    Update ProgressBar.cs

--- The World of Small Animals/Assets/Scripts/Others/UI/ProgressBar.cs
+++ The World of Small Animals/Assets/Scripts/Others/UI/ProgressBar.cs
@@ -4,30 +4,30 @@ using UnityEngine.UI;
 
     public class ProgressBar : MonoBehaviour
     {
-        protected Slider slider;
+        protected Slider _slider;
 
 
         [Header("Плавное изменение полоски")]
-        [SerializeField] private bool lerpingValue = false;
+        [SerializeField] private bool _lerpingValue = false;
 
     [Header("Скорость плавного изменения значения")]
-    [SerializeField] private float lerpingSpeed = 0;
+    [SerializeField] private float _lerpingSpeed = 0;
 
-    public bool LerpingValue { get => lerpingValue; }
-    public float LerpingSpeed { get => lerpingSpeed; }
+    public bool LerpingValue { get => _lerpingValue; }
+    public float LerpingSpeed { get => _lerpingSpeed; }
 
-    public float Value { get => slider.value; }
+    public float Value { get => _slider.value; }
 
 
     protected void Ini ()
         {
-        if (LerpingValue && LerpingSpeed <= 0)
+        if (_LerpingValue && _LerpingSpeed <= 0)
         {
             throw new ProgressBarException("lerping speed <= 0");
         }
 
 
-        if (!TryGetComponent(out slider))
+        if (!TryGetComponent(out _slider))
             {
                 throw new ProgressBarException($"{name} not have component Slider");
             }
